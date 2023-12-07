95e1ea926c55ca082de36c1891c0b0d7fcddc463
GeorgeK0u <144594716+GeorgeK0u@users.noreply.github.com>
2023-12-07 18:50:10 +0200

    Show search results

--- script.js
+++ script.js
@@ -9,21 +9,42 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 const searchInput = document.querySelector('#search-input');
 const searchBtn = document.querySelector('#search-btn');
+const videosWrapper = document.querySelector('#videos-wrapper');
 searchBtn.onclick = (e) => __awaiter(this, void 0, void 0, function* () {
     e.preventDefault();
     const query = searchInput.value;
     if (query != '') {
+        // remove previous search results
+        for (let i = videosWrapper.childElementCount - 1; i >= 0; i--) {
+            videosWrapper.children[i].remove();
+        }
         // Update search results
         try {
-            const maxResults = 50;
+            const maxResults = 10;
             const apiKey = 'AIzaSyDrn07slgPiKCk-HzkTQWTH4yl2PEOs51w';
             const resp = yield fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${maxResults}&q=${query}&key=${apiKey}`);
             const jsonResp = yield resp.json();
             console.log('Updating search results');
             console.log(jsonResp);
+            // Show search results
+            const searchResults = jsonResp.items;
+            for (let i = 0; i < searchResults.length; i++) {
+                const videoData = searchResults[i].snippet;
+                const videoWrapper = document.createElement('div');
+                videoWrapper.className = 'video-wrapper';
+                const videoTitle = document.createElement('div');
+                videoTitle.className = 'video-title';
+                videoTitle.textContent = videoData.title;
+                const videoThumbnail = document.createElement('img');
+                videoThumbnail.className = 'video-thumbnail';
+                videoThumbnail.src = videoData.thumbnails.default.url;
+                videoWrapper.appendChild(videoTitle);
+                videoWrapper.appendChild(videoThumbnail);
+                videosWrapper.appendChild(videoWrapper);
+            }
         }
         catch (err) {
-            console.log(err);
+            console.log('Exception occured: ', err);
         }
     }
     return;
